9aa6e133662b5ea02e3ca0e948dea93be9881c2b
Hugues Romain <huguesromainpro@gmail.com>
2019-11-20 11:08:39 +0100

    fix blocking scroll

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
+/* 
+Cette variable représente la valeur du scroll, elle est à `false` de base 
+étant donné que l'utilisateur n'est pas censé pouvoir scroller en arrivant sur le 
+site 
+*/
+var checkScroll = false;
+
+/* 
+je viens initier la fonction vérifiant la valeur de du scroll grace a 
+`checkScroll` et en positionnant l'overflow a scroll si `checkScroll` est à `true` 
+et `hidden` si `checkScroll` est à `false`. 
+(https://developer.mozilla.org/fr/docs/Web/CSS/overflow)
+*/
+const blockScroll = () => {
+  if (checkScroll) {
+    document.documentElement.style.overflow = "scroll";
+  } else if (!checkScroll) {
+    document.documentElement.style.overflow = "hidden";
+  }
+};
+
+// j'appelle la fonction une première fois ici
+blockScroll();
+//
+
 var btn = document.querySelector(".toggle_btn");
 var nav = document.querySelector(".nav");
-
-btn.onclick = function() {
-  nav.classList.toggle("nav_open");
-};
-//scroll taptoswap//
 var tap = document.querySelector(".noscroll");
 
 tap.onclick = function() {
   tap.classList.add("tapswap");
+  /* 
+Au click sur `.noscroll`, je viens définir `checkScroll` à `true` et je re appel 
+la fonction qui du coup initie `overflow` en `scroll` qui libére le scroll
+ */
+  checkScroll = true;
+  blockScroll();
+  //
 };
+
 //reveals img by change opacity//
 
 function revealImagesInViewport() {
