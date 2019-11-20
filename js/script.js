555ed4deceae82ee348422edf41dd7b3fe3d5eb7
Hugues Romain <huguesromainpro@gmail.com>
2019-11-20 15:20:51 +0100

    repaire burger

--- js/script.js
+++ js/script.js
@@ -27,6 +27,10 @@ var btn = document.querySelector(".toggle_btn");
 var nav = document.querySelector(".nav");
 var tap = document.querySelector(".noscroll");
 
+btn.onclick = function() {
+  nav.classList.toggle("nav_open");
+};
+
 tap.onclick = function() {
   tap.classList.add("tapswap");
   /* 
